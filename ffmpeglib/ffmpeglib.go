93f678bae8cac17519a0fca14b051c8c52390f6a
Umer <muhammad.umer@mslm.io>
2024-01-01 14:42:57 +0500

    command passed in strings

--- ffmpeglib/ffmpeglib.go
+++ ffmpeglib/ffmpeglib.go
@@ -10,13 +10,27 @@ import (
 
 // MP4toH264Converter represents the MP4 to H.264 converter.
 type MP4toH264Converter struct {
-	InputFile string
+	InputFile    string
+	VideoCodec   string
+	Preset       string
+	CRF          string
+	AudioCodec   string
+	Strict       string
+	AudioBitrate string
+	Threads      string
 }
 
 // NewConverter creates a new MP4toH264Converter instance.
 func NewConverter(inputFile string) *MP4toH264Converter {
 	return &MP4toH264Converter{
-		InputFile: inputFile,
+		InputFile:    inputFile,
+		VideoCodec:   "libx264",
+		Preset:       "fast",
+		CRF:          "23",
+		AudioCodec:   "aac",
+		Strict:       "experimental",
+		AudioBitrate: "192k",
+		Threads:      "4",
 	}
 }
 
@@ -34,13 +48,13 @@ func (c *MP4toH264Converter) Convert() (string, error) {
 	cmd := exec.Command(
 		"/usr/bin/ffmpeg",
 		"-i", c.InputFile,
-		"-c:v", "libx264", // H.264 codec
-		"-preset", "fast", // Choose a preset for encoding speed vs compression ratio
-		"-crf", "23", // Constant Rate Factor (0-51, lower is better quality)
-		"-c:a", "aac", // AAC audio codec
-		"-strict", "experimental", // Needed for older FFmpeg versions
-		"-b:a", "128k", // Set audio bitrate (adjust as needed)
-		"-threads", "4", // Adjust the number based on your CPU core count
+		"-c:v", c.VideoCodec,
+		"-preset", c.Preset,
+		"-crf", c.CRF,
+		"-c:a", c.AudioCodec,
+		"-strict", c.Strict,
+		"-b:a", c.AudioBitrate,
+		"-threads", c.Threads,
 		outputFileName,
 	)
 
